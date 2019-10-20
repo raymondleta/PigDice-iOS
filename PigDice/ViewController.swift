5af0903c30bd042e5c658322487342879a553493
raymond gitonga <raytosh95@gmail.com>
2019-10-20 23:17:56 +0300

    Add randomization

--- PigDice/ViewController.swift
+++ PigDice/ViewController.swift
@@ -14,25 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var rightDiceView: UIImageView!
     @IBOutlet weak var leftDiceView: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-       
-        
-    }
-
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-       leftDiceView.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][leftDiceNumber]
-       rightDiceView.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][rightDiceNumber]
         
-        leftDiceNumber+=1
-        rightDiceNumber-=1
-        
+       let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+       leftDiceView.image = diceArray.randomElement()
+       rightDiceView.image = diceArray.randomElement()
         
         Loaf("Button clicked", state: .success, sender: self).show()
         
